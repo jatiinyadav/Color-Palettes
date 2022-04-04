78d188be6b4ac950bdfe33c065a117436163ea58
jatiinyadav <jatin27yadav@gmail.com>
2022-04-04 21:20:05 +0530

    Minor changes, More need to be done

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ close.addEventListener("click", () => {
 });
 
 // BUTTON
-for (let i = 1; i <= 80; i++) {
+for (let i = 1; i <= 25; i++) {
   const box = document.createElement("div");
   box.classList.add("box");
   document.querySelector(".container").appendChild(box);
