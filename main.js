d40adb6dafb84afe8d00a157dd4b564c8d9b3b62
HRC-123 <106550364+HRC-123@users.noreply.github.com>
2023-10-07 20:11:31 +0530

    added copy function
    Now users can copy the color.

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ window.onload = function () {
 // BUTTON
 for (let i = 1; i <= 25; i++) {
   const box = document.createElement("div");
+  
   box.classList.add("box");
   document.querySelector(".container").appendChild(box);
 }
 
 const btn = document.querySelector(".btn");
 const randomColorBlock = document.querySelectorAll(".box");
+
 
 function RandomHexColorCode() {
   var chars = "0123456789abcdef";
@@ -30,5 +32,23 @@ function addColor() {
     var newColor = RandomHexColorCode();
     e.style.backgroundColor = newColor;
     e.innerHTML = newColor;
+
+    // Get the element you want to add hover styles to
+    // Add a 'mouseenter' event listener to add the 'hovered' class when the mouse enters
+    e.addEventListener('mouseenter', () => {
+      // console.log('hovered');
+    e.innerHTML="copy";
+    });
+    // Add a 'mouseleave' event listener to remove the 'hovered' class when the mouse leaves
+    e.addEventListener('mouseleave', () => {
+      // console.log('nothovered');
+      e.innerHTML=newColor;
+    });
+
+    e.addEventListener('click',()=>{
+      navigator.clipboard.writeText(newColor);
+    });
+
+    
   });
 }
